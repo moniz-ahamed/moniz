b871abc6b7abca53f272fc819c2c40196366221d
moniz-ahamed <monizhasan@gmail.com>
2025-02-25 18:17:42 +0600

    Django template For Loop

--- moniztech/views.py
+++ moniztech/views.py
@@ -1,11 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-def homePage (request):
-    return render (request, "index.html")
 
-def aboutUs (request):
-    return HttpResponse("welcome to moniztech")
-def course (request):
-    return HttpResponse("<b>welcome to moniztech_course<b/>")
-def courseDetails (request, courseid):
-    return HttpResponse (courseid)
+def homePage(request):
+    data = {
+        'title': 'My Courses',
+        'clist': ['PHP', 'Java', 'Django'],
+        'course_details': [
+            {'course': 'PHP', 'price': '$100'},
+            {'course': 'Java', 'price': '$200'},
+            {'course': 'Django', 'price': '$300'}
+        ]
+    }
+    return render(request, "index.html", data)
+
+def aboutUs(request):
+    return HttpResponse("Welcome to MonizTech")
+
+def course(request):
+    return HttpResponse("<b>Welcome to MonizTech Course<b/>")
+
+def courseDetails(request, courseid):
+    return HttpResponse(courseid)
